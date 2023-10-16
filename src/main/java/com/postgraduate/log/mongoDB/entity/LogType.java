87d44399c74f220939b29b9fadd66fbeac609316
yang <ywj9811@gmail.com>
2023-10-17 02:47:15 +0900

    feat: Log Setting

---
+++ src/main/java/com/postgraduate/log/mongoDB/entity/LogType.java
@@ -0,0 +1,5 @@
+package com.postgraduate.log.mongoDB.entity;
+
+public enum LogType {
+    INFO, WARN, ERROR
+}
---
+++ src/main/java/com/postgraduate/log/rabbitMQ/mqConsumer/MessageConsumer.java
@@ -0,0 +1,25 @@
+package com.postgraduate.log.rabbitMQ.mqConsumer;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.postgraduate.log.mongoDB.dto.LogRequest;
+import com.postgraduate.log.mongoDB.repository.LogRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class MessageConsumer {
+    private final LogRepository logRepository;
+    private final ObjectMapper objectMapper;
+
+    @RabbitListener(queues = "${spring.rabbitmq.queue.name}")
+    public void receive(String request) throws JsonProcessingException {
+        LogRequest logRequest = objectMapper.readValue(request, LogRequest.class);
+        log.info("Received message: {}", logRequest.toString());
+        logRepository.save(logRequest.toEntity());
+    }
+}
